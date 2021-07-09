09a34618cb25b15f77dab901fabf1fc4e30a4f2e
Ricardo Sander Lopes <ricardo.lopes@ifood.com.br>
2021-07-08 23:02:06 -0300

    Adding fluent assert from assertj instead of junit asserts.

--- application/src/test/java/io/github/ricardosander/jusbrasilprocessservice/application/RetrieveProcessUseCaseTest.java
+++ application/src/test/java/io/github/ricardosander/jusbrasilprocessservice/application/RetrieveProcessUseCaseTest.java
@@ -2,13 +2,13 @@ package io.github.ricardosander.jusbrasilprocessservice.application;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class RetrieveProcessUseCaseTest {
 
     @Test
     void test() {
         new RetrieveProcessUseCase();
-        assertTrue(true);
+        assertThat(true).isTrue();
     }
 }
