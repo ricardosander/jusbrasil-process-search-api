f2d277e058895b2137d6691f8e717938b7c84531
Ricardo Sander Lopes <ricardo.lopes@ifood.com.br>
2021-07-09 01:30:24 -0300

    Adds TJAL example unit test.

--- application/src/main/java/io/github/ricardosander/jusbrasilprocessservice/application/RetrieveProcessUseCase.java
+++ application/src/main/java/io/github/ricardosander/jusbrasilprocessservice/application/RetrieveProcessUseCase.java
@@ -1,9 +1,60 @@
 package io.github.ricardosander.jusbrasilprocessservice.application;
 
+import java.util.Optional;
+
 public class RetrieveProcessUseCase {
 
-    public void execute(String uniqueProcessNumbering) {
-        throw new InvalidUniqueProcessNumberingException(uniqueProcessNumbering);
+    public Optional<Process> execute(String uniqueProcessNumbering) {
+        if (uniqueProcessNumbering == null || uniqueProcessNumbering.trim().isEmpty()) {
+            throw new InvalidUniqueProcessNumberingException(uniqueProcessNumbering);
+        }
+
+        return Optional.of(new Process(uniqueProcessNumbering));
+    }
+
+    static class Process {
+
+        private final String id;
+
+        Process(String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getClazz() {
+            return "Procedimento Comum Cível";
+        }
+
+        public String getArea() {
+            return "Cível";
+        }
+
+        public String getSubject() {
+            return "Dano Material";
+        }
+
+        public String getDistributionDate() {
+            return "02/05/2018 às 19:01 - Sorteio";
+        }
+
+        public String getJudge() {
+            return "José Cícero Alves da Silva";
+        }
+
+        public String getShareValue() {
+            return "R$ 281.178,42";
+        }
+
+        public String getProcessParts() {
+            return "Autor: José Carlos Cerqueira Souza Filho\nAdvogado:  Vinicius Faria de Cerqueira\nRé: Cony Engenharia Ltda.\nAdvogado:  Carlos Henrique de Mendonça Brandão\nAdvogado:  Guilherme Freire Furtado\nAdvogada:  Maria Eugênia Barreiros de Mello\nAdvogado:  Vítor Reis de Araujo Carvalho";
+        }
+
+        public String getMovements() {
+            return "22/02/2021 - Remetido recurso eletrônico ao Tribunal de Justiça/Turma de recurso";
+        }
     }
 
     static class InvalidUniqueProcessNumberingException extends RuntimeException {
--- application/src/test/java/io/github/ricardosander/jusbrasilprocessservice/application/RetrieveProcessUseCaseTest.java
+++ application/src/test/java/io/github/ricardosander/jusbrasilprocessservice/application/RetrieveProcessUseCaseTest.java
@@ -3,6 +3,9 @@ package io.github.ricardosander.jusbrasilprocessservice.application;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class RetrieveProcessUseCaseTest {
@@ -12,6 +15,26 @@ class RetrieveProcessUseCaseTest {
     @BeforeEach
     void setUp() {
         retrieveProcessUseCase = new RetrieveProcessUseCase();
+    }
+
+    @Test
+    void shouldReturnProcess_whenTJALUniqueProcessNumberingIsGiven() {
+
+        String uniqueProcessNumbering = "07108025520188020001";
+
+        Optional<RetrieveProcessUseCase.Process> process = retrieveProcessUseCase.execute(uniqueProcessNumbering);
+
+        assertThat(process).isPresent();
+        assertThat(process.get().getId()).isEqualTo(uniqueProcessNumbering);
+        assertThat(process.get().getClazz()).isEqualTo("Procedimento Comum Cível");
+        assertThat(process.get().getArea()).isEqualTo("Cível");
+        assertThat(process.get().getSubject()).isEqualTo("Dano Material");
+        assertThat(process.get().getDistributionDate()).isEqualTo("02/05/2018 às 19:01 - Sorteio");
+        assertThat(process.get().getJudge()).isEqualTo("José Cícero Alves da Silva");
+        assertThat(process.get().getShareValue()).isEqualTo("R$ 281.178,42");
+        assertThat(process.get().getShareValue()).isEqualTo("R$ 281.178,42");
+        assertThat(process.get().getProcessParts()).isEqualTo("Autor: José Carlos Cerqueira Souza Filho\nAdvogado:  Vinicius Faria de Cerqueira\nRé: Cony Engenharia Ltda.\nAdvogado:  Carlos Henrique de Mendonça Brandão\nAdvogado:  Guilherme Freire Furtado\nAdvogada:  Maria Eugênia Barreiros de Mello\nAdvogado:  Vítor Reis de Araujo Carvalho");
+        assertThat(process.get().getMovements()).isEqualTo("22/02/2021 - Remetido recurso eletrônico ao Tribunal de Justiça/Turma de recurso");
     }
 
     @Test
