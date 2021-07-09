09279883272258b8f3cb828c277d8e42d8c62bd9
Ricardo Sander Lopes <ricardo.lopes@ifood.com.br>
2021-07-08 22:47:38 -0300

    Creates first failing test.

---
+++ application/src/test/java/JusbrasilProcessServiceTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class JusbrasilProcessServiceTest {
+
+    @Test
+    void test() {
+        assertTrue(false);
+    }
+}
