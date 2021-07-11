f2655b18642cccc3192571854307b9a0f0f9c47a
Ricardo Sander Lopes <ricardo.lopes@ifood.com.br>
2021-07-10 22:20:26 -0300

    Refactor: extracts data creation code to methods.

--- application/src/test/java/io/github/ricardosander/jusbrasilprocessservice/application/RetrieveProcessUseCaseTest.java
+++ application/src/test/java/io/github/ricardosander/jusbrasilprocessservice/application/RetrieveProcessUseCaseTest.java
@@ -48,19 +48,16 @@ class RetrieveProcessUseCaseTest {
         assertThat(process.get().getShareValue()).isEqualTo("R$ 281.178,42");
         assertThat(process.get().getProcessParts()).hasSize(7);
         assertThat(process.get().getProcessParts()).contains(
-                new ProcessPart("Autor", "José Carlos Cerqueira Souza Filho"),
-                new ProcessPart("Autor", "Advogado", "Vinicius Faria de Cerqueira"),
-                new ProcessPart("Ré", "Cony Engenharia Ltda."),
-                new ProcessPart("Ré", "Advogado", "Carlos Henrique de Mendonça Brandão"),
-                new ProcessPart("Ré", "Advogado", "Guilherme Freire Furtado"),
-                new ProcessPart("Ré", "Advogada", "Maria Eugênia Barreiros de Mello"),
-                new ProcessPart("Ré", "Advogado", "Vítor Reis de Araujo Carvalho")
+                createsProcessPart("Autor", "José Carlos Cerqueira Souza Filho"),
+                createsProcessPart("Autor", "Advogado", "Vinicius Faria de Cerqueira"),
+                createsProcessPart("Ré", "Cony Engenharia Ltda."),
+                createsProcessPart("Ré", "Advogado", "Carlos Henrique de Mendonça Brandão"),
+                createsProcessPart("Ré", "Advogado", "Guilherme Freire Furtado"),
+                createsProcessPart("Ré", "Advogada", "Maria Eugênia Barreiros de Mello"),
+                createsProcessPart("Ré", "Advogado", "Vítor Reis de Araujo Carvalho")
         );
         assertThat(process.get().getMovements()).contains(
-                new Movement(
-                        LocalDate.parse("22/02/2021", DATE_FORMATTER),
-                        "Remetido recurso eletrônico ao Tribunal de Justiça/Turma de recurso"
-                )
+                createsProcessMovement("22/02/2021", "Remetido recurso eletrônico ao Tribunal de Justiça/Turma de recurso")
         );
     }
 
@@ -81,17 +78,14 @@ class RetrieveProcessUseCaseTest {
         assertThat(process.get().getShareValue()).isEqualTo("R$ 10.000,00");
         assertThat(process.get().getProcessParts()).hasSize(5);
         assertThat(process.get().getProcessParts()).contains(
-                new ProcessPart("Autora", "Leidi Silva Ormond Galvão"),
-                new ProcessPart("Autora", "Advogada", "Adriana Catelan Skowronski"),
-                new ProcessPart("Autora", "Advogada", "Ana Silvia Pessoa Salgado de Moura"),
-                new ProcessPart("Réu", "Estado de Mato Grosso do Sul"),
-                new ProcessPart("Réu", "RepreLeg", "Procuradoria Geral do Estado de Mato Grosso do Sul")
+                createsProcessPart("Autora", "Leidi Silva Ormond Galvão"),
+                createsProcessPart("Autora", "Advogada", "Adriana Catelan Skowronski"),
+                createsProcessPart("Autora", "Advogada", "Ana Silvia Pessoa Salgado de Moura"),
+                createsProcessPart("Réu", "Estado de Mato Grosso do Sul"),
+                createsProcessPart("Réu", "RepreLeg", "Procuradoria Geral do Estado de Mato Grosso do Sul")
         );
         assertThat(process.get().getMovements()).contains(
-                new Movement(
-                        LocalDate.parse("17/07/2020", DATE_FORMATTER),
-                        "Guia de Recolhimento Judicial Emitida"
-                )
+                createsProcessMovement("17/07/2020", "Guia de Recolhimento Judicial Emitida")
         );
     }
 
@@ -124,47 +118,50 @@ class RetrieveProcessUseCaseTest {
 
     private Map<String, Process> createProcesses() {
         return Map.of(
-                "07108025520188020001", createProcess(
-                        "Dano Material",
-                        LocalDateTime.parse("02/05/2018 19:01", DATE_TIME_FORMATTER),
-                        "Sorteio",
-                        "José Cícero Alves da Silva",
-                        "R$ 281.178,42",
-                        List.of(
-                                new ProcessPart("Autor", "José Carlos Cerqueira Souza Filho"),
-                                new ProcessPart("Autor", "Advogado", "Vinicius Faria de Cerqueira"),
-                                new ProcessPart("Ré", "Cony Engenharia Ltda."),
-                                new ProcessPart("Ré", "Advogado", "Carlos Henrique de Mendonça Brandão"),
-                                new ProcessPart("Ré", "Advogado", "Guilherme Freire Furtado"),
-                                new ProcessPart("Ré", "Advogada", "Maria Eugênia Barreiros de Mello"),
-                                new ProcessPart("Ré", "Advogado", "Vítor Reis de Araujo Carvalho")
-                        ),
-                        List.of(
-                                new Movement(
-                                        LocalDate.parse("22/02/2021", DATE_FORMATTER),
-                                        "Remetido recurso eletrônico ao Tribunal de Justiça/Turma de recurso"
-                                )
-                        )
+                "07108025520188020001", createsTJALProcess(),
+                "08219015120188120001", createsTMJSProcess()
+        );
+    }
+
+    private Process createsTJALProcess() {
+        return createProcess(
+                "Dano Material",
+                createsDateTime("02/05/2018 19:01"),
+                "Sorteio",
+                "José Cícero Alves da Silva",
+                "R$ 281.178,42",
+                List.of(
+                        createsProcessPart("Autor", "José Carlos Cerqueira Souza Filho"),
+                        createsProcessPart("Autor", "Advogado", "Vinicius Faria de Cerqueira"),
+                        createsProcessPart("Ré", "Cony Engenharia Ltda."),
+                        createsProcessPart("Ré", "Advogado", "Carlos Henrique de Mendonça Brandão"),
+                        createsProcessPart("Ré", "Advogado", "Guilherme Freire Furtado"),
+                        createsProcessPart("Ré", "Advogada", "Maria Eugênia Barreiros de Mello"),
+                        createsProcessPart("Ré", "Advogado", "Vítor Reis de Araujo Carvalho")
                 ),
-                "08219015120188120001", createProcess(
-                        "Enquadramento",
-                        LocalDateTime.parse("30/07/2018 12:39", DATE_TIME_FORMATTER),
-                        "Automática",
-                        "Fernando Paes de Campos",
-                        "R$ 10.000,00",
-                        List.of(
-                                new ProcessPart("Autora", "Leidi Silva Ormond Galvão"),
-                                new ProcessPart("Autora", "Advogada", "Adriana Catelan Skowronski"),
-                                new ProcessPart("Autora", "Advogada", "Ana Silvia Pessoa Salgado de Moura"),
-                                new ProcessPart("Réu", "Estado de Mato Grosso do Sul"),
-                                new ProcessPart("Réu", "RepreLeg", "Procuradoria Geral do Estado de Mato Grosso do Sul")
-                        ),
-                        List.of(
-                                new Movement(
-                                        LocalDate.parse("17/07/2020", DATE_FORMATTER),
-                                        "Guia de Recolhimento Judicial Emitida"
-                                )
-                        )
+                List.of(createsProcessMovement(
+                        "22/02/2021",
+                        "Remetido recurso eletrônico ao Tribunal de Justiça/Turma de recurso"
+                ))
+        );
+    }
+
+    private Process createsTMJSProcess() {
+        return createProcess(
+                "Enquadramento",
+                createsDateTime("30/07/2018 12:39"),
+                "Automática",
+                "Fernando Paes de Campos",
+                "R$ 10.000,00",
+                List.of(
+                        createsProcessPart("Autora", "Leidi Silva Ormond Galvão"),
+                        createsProcessPart("Autora", "Advogada", "Adriana Catelan Skowronski"),
+                        createsProcessPart("Autora", "Advogada", "Ana Silvia Pessoa Salgado de Moura"),
+                        createsProcessPart("Réu", "Estado de Mato Grosso do Sul"),
+                        createsProcessPart("Réu", "RepreLeg", "Procuradoria Geral do Estado de Mato Grosso do Sul")
+                ),
+                List.of(
+                        createsProcessMovement("17/07/2020", "Guia de Recolhimento Judicial Emitida")
                 )
         );
     }
@@ -182,11 +179,35 @@ class RetrieveProcessUseCaseTest {
                 "Procedimento Comum Cível",
                 "Cível",
                 subject,
-                new DistributionDate(distributionDate, distributionType),
+                createsDistributionDate(distributionDate, distributionType),
                 judge,
                 shareValue,
                 processParts,
                 movements
         );
+    }
+
+    private LocalDateTime createsDateTime(String date) {
+        return LocalDateTime.parse(date, DATE_TIME_FORMATTER);
+    }
+
+    private ProcessPart createsProcessPart(String type, String part) {
+        return new ProcessPart(type, part);
+    }
+
+    private ProcessPart createsProcessPart(String type, String subType, String part) {
+        return new ProcessPart(type, subType, part);
+    }
+
+    private Movement createsProcessMovement(String movementDate, String movementDescription) {
+        return new Movement(createsDate(movementDate), movementDescription);
+    }
+
+    private DistributionDate createsDistributionDate(LocalDateTime distributionDate, String distributionType) {
+        return new DistributionDate(distributionDate, distributionType);
+    }
+
+    private LocalDate createsDate(String date) {
+        return LocalDate.parse(date, DATE_FORMATTER);
     }
 }
