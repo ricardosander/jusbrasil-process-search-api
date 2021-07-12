15c2d93b7c9490372f08c339c3fc8dda82aa0a5b
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2021-07-12 00:32:53 -0300

    Fixes validator digit on tests inputs.

--- application/src/test/java/io/github/ricardosander/jusbrasilprocessservice/application/RetrieveProcessUseCaseTest.java
+++ application/src/test/java/io/github/ricardosander/jusbrasilprocessservice/application/RetrieveProcessUseCaseTest.java
@@ -129,7 +129,7 @@ class RetrieveProcessUseCaseTest {
     @Test
     void shouldThrowsInvalidUniqueProcessNumberingException_whenGivenUniqueProcessNumberingHasInvalidTR() {
 
-        String uniqueProcessNumbering = "08219015120188280001";
+        String uniqueProcessNumbering = "08219010720188280001";
 
         assertThatExceptionOfType(InvalidUniqueProcessNumberingException.class)
             .isThrownBy(() -> retrieveProcessUseCase.execute(uniqueProcessNumbering));
@@ -147,7 +147,7 @@ class RetrieveProcessUseCaseTest {
     @Test
     void shouldThrowsInvalidUniqueProcessNumberingException_whenGivenUniqueProcessNumberingJIsInvalid() {
 
-        String uniqueProcessNumbering = "08219015120180120001";
+        String uniqueProcessNumbering = "08219012020180120001";
 
         assertThatExceptionOfType(InvalidUniqueProcessNumberingException.class)
             .isThrownBy(() -> retrieveProcessUseCase.execute(uniqueProcessNumbering));
@@ -166,7 +166,7 @@ class RetrieveProcessUseCaseTest {
     void shouldThrowsInvalidUniqueProcessNumberingException_whenGivenUniqueProcessNumberingJIHasFutureYear() {
 
         int futureYear = LocalDate.now().getYear() + 1;
-        String uniqueProcessNumbering = String.format("082190151%s8120001", futureYear);
+        String uniqueProcessNumbering = String.format("082190112%s8120001", futureYear);
 
         assertThatExceptionOfType(InvalidUniqueProcessNumberingException.class)
             .isThrownBy(() -> retrieveProcessUseCase.execute(uniqueProcessNumbering));
@@ -175,7 +175,7 @@ class RetrieveProcessUseCaseTest {
     @Test
     void shouldThrowsInvalidUniqueProcessNumberingException_whenGivenUniqueProcessNumberingJIHasYearBeforeRepublic() {
 
-        String uniqueProcessNumbering = "08219015118888120001";
+        String uniqueProcessNumbering = "08219010918888120001";
 
         assertThatExceptionOfType(InvalidUniqueProcessNumberingException.class)
             .isThrownBy(() -> retrieveProcessUseCase.execute(uniqueProcessNumbering));
@@ -184,7 +184,7 @@ class RetrieveProcessUseCaseTest {
     @Test
     void shouldThrowsInvalidUniqueProcessNumberingException_whenGivenUniqueProcessNumberingHasInvalidNumber() {
 
-        String uniqueProcessNumbering = "00000005120188120001";
+        String uniqueProcessNumbering = "00000006020188120001";
 
         assertThatExceptionOfType(InvalidUniqueProcessNumberingException.class)
             .isThrownBy(() -> retrieveProcessUseCase.execute(uniqueProcessNumbering));
