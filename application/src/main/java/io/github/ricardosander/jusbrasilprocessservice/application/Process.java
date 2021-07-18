5fc6979bf446d10e82ca1c117e3a13ed2973e66c
Ricardo Sander Lopes <ricardo.lopes@ifood.com.br>
2021-07-18 00:44:36 -0300

    Creates Process Builder to encapsulate package classes and keep their scope.

--- application/src/main/java/io/github/ricardosander/jusbrasilprocessservice/application/Process.java
+++ application/src/main/java/io/github/ricardosander/jusbrasilprocessservice/application/Process.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -18,4 +21,83 @@ public class Process {
     private final String shareValue;
     private final List<ProcessPart> processParts;
     private final List<Movement> movements;
+
+    public static class Builder {
+
+        private String clazz;
+        private String area;
+        private String subject;
+        private DistributionDate distributionDate;
+        private String judge;
+        private String shareValue;
+        private final List<ProcessPart> processParts;
+        private final List<Movement> movements;
+
+        private Builder() {
+            this.processParts = new ArrayList<>();
+            this.movements = new ArrayList<>();
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public Builder clazz(String clazz) {
+            this.clazz = clazz;
+            return this;
+        }
+
+        public Builder area(String area) {
+            this.area = area;
+            return this;
+        }
+
+        public Builder subject(String subject) {
+            this.subject = subject;
+            return this;
+        }
+
+        public Builder distributionDate(LocalDateTime distributionDate, String distributionDateType) {
+            this.distributionDate = new DistributionDate(distributionDate, distributionDateType);
+            return this;
+        }
+
+        public Builder judge(String judge) {
+            this.judge = judge;
+            return this;
+        }
+
+        public Builder shareValue(String shareValue) {
+            this.shareValue = shareValue;
+            return this;
+        }
+
+        public Builder processParts(String type, String part) {
+            this.processParts.add(new ProcessPart(type, part));
+            return this;
+        }
+
+        public Builder processParts(String type, String subType, String part) {
+            this.processParts.add(new ProcessPart(type, subType, part));
+            return this;
+        }
+
+        public Builder movements(LocalDate date, String description) {
+            this.movements.add(new Movement(date, description));
+            return this;
+        }
+
+        public Process build() {
+            return new Process(
+              this.clazz,
+              this.area,
+              this.subject,
+              this.distributionDate,
+              this.judge,
+              this.shareValue,
+              this.processParts,
+              this.movements
+            );
+        }
+    }
 }
--- application/src/test/java/io/github/ricardosander/jusbrasilprocessservice/application/ProcessFixtureFactory.java
+++ application/src/test/java/io/github/ricardosander/jusbrasilprocessservice/application/ProcessFixtureFactory.java
@@ -3,112 +3,75 @@ package io.github.ricardosander.jusbrasilprocessservice.application;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Map;
 
 public class ProcessFixtureFactory {
 
-  private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-  private static final DateTimeFormatter DATE_TIME_FORMATTER =
-      DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm");
 
-  private ProcessFixtureFactory() {
-  }
+    private ProcessFixtureFactory() {
+    }
 
-  static Map<String, Process> createProcesses() {
-    return Map.of(
-        "07108025520188020001", createsTJALProcess(),
-        "08219015120188120001", createsTJMSProcess()
-    );
-  }
+    static Map<String, Process> createProcesses() {
+        return Map.of(
+                "07108025520188020001", createsTJALProcess(),
+                "08219015120188120001", createsTJMSProcess()
+        );
+    }
 
-  static Process createsTJALProcess() {
-    return createProcess(
-        "Dano Material",
-        createsDateTime("02/05/2018 19:01"),
-        "Sorteio",
-        "José Cícero Alves da Silva",
-        "R$ 281.178,42",
-        List.of(
-            createsProcessPart("Autor", "José Carlos Cerqueira Souza Filho"),
-            createsProcessPart("Autor", "Advogado", "Vinicius Faria de Cerqueira"),
-            createsProcessPart("Ré", "Cony Engenharia Ltda."),
-            createsProcessPart("Ré", "Advogado", "Carlos Henrique de Mendonça Brandão"),
-            createsProcessPart("Ré", "Advogado", "Guilherme Freire Furtado"),
-            createsProcessPart("Ré", "Advogada", "Maria Eugênia Barreiros de Mello"),
-            createsProcessPart("Ré", "Advogado", "Vítor Reis de Araujo Carvalho")
-        ),
-        List.of(createsProcessMovement(
-            "22/02/2021",
-            "Remetido recurso eletrônico ao Tribunal de Justiça/Turma de recurso"
-        ))
-    );
-  }
+    static Process createsTJALProcess() {
+        return Process.Builder.builder()
+                .clazz("Procedimento Comum Cível")
+                .area("Cível")
+                .subject("Dano Material")
+                .judge("José Cícero Alves da Silva")
+                .distributionDate(
+                        createsDateTime("02/05/2018 19:01"),
+                        "Sorteio"
+                )
+                .shareValue("R$ 281.178,42")
+                .processParts("Autor", "José Carlos Cerqueira Souza Filho")
+                .processParts("Autor", "Advogado", "Vinicius Faria de Cerqueira")
+                .processParts("Ré", "Cony Engenharia Ltda.")
+                .processParts("Ré", "Advogado", "Carlos Henrique de Mendonça Brandão")
+                .processParts("Ré", "Advogado", "Guilherme Freire Furtado")
+                .processParts("Ré", "Advogada", "Maria Eugênia Barreiros de Mello")
+                .processParts("Ré", "Advogado", "Vítor Reis de Araujo Carvalho")
+                .movements(
+                        createsDate("22/02/2021"),
+                        "Remetido recurso eletrônico ao Tribunal de Justiça/Turma de recurso"
+                ).build();
+    }
 
-  static Process createsTJMSProcess() {
-    return createProcess(
-        "Enquadramento",
-        createsDateTime("30/07/2018 12:39"),
-        "Automática",
-        "Fernando Paes de Campos",
-        "R$ 10.000,00",
-        List.of(
-            createsProcessPart("Autora", "Leidi Silva Ormond Galvão"),
-            createsProcessPart("Autora", "Advogada", "Adriana Catelan Skowronski"),
-            createsProcessPart("Autora", "Advogada", "Ana Silvia Pessoa Salgado de Moura"),
-            createsProcessPart("Réu", "Estado de Mato Grosso do Sul"),
-            createsProcessPart("Réu", "RepreLeg",
-                "Procuradoria Geral do Estado de Mato Grosso do Sul")
-        ),
-        List.of(
-            createsProcessMovement("17/07/2020", "Guia de Recolhimento Judicial Emitida")
-        )
-    );
-  }
+    static Process createsTJMSProcess() {
+        return Process.Builder.builder()
+                .clazz("Procedimento Comum Cível")
+                .area("Cível")
+                .subject("Enquadramento")
+                .judge("Fernando Paes de Campos")
+                .distributionDate(
+                        createsDateTime("30/07/2018 12:39"),
+                        "Automática"
+                )
+                .shareValue("R$ 10.000,00")
+                .processParts("Autora", "Leidi Silva Ormond Galvão")
+                .processParts("Autora", "Advogada", "Adriana Catelan Skowronski")
+                .processParts("Autora", "Advogada", "Ana Silvia Pessoa Salgado de Moura")
+                .processParts("Réu", "Estado de Mato Grosso do Sul")
+                .processParts("Réu", "RepreLeg", "Procuradoria Geral do Estado de Mato Grosso do Sul")
+                .movements(
+                        createsDate("17/07/2020"),
+                        "Guia de Recolhimento Judicial Emitida"
+                ).build();
+    }
 
-  private static Process createProcess(
-      String subject,
-      LocalDateTime distributionDate,
-      String distributionType,
-      String judge,
-      String shareValue,
-      List<ProcessPart> processParts,
-      List<Movement> movements
-  ) {
-    return new Process(
-        "Procedimento Comum Cível",
-        "Cível",
-        subject,
-        createsDistributionDate(distributionDate, distributionType),
-        judge,
-        shareValue,
-        processParts,
-        movements
-    );
-  }
+    private static LocalDateTime createsDateTime(String date) {
+        return LocalDateTime.parse(date, DATE_TIME_FORMATTER);
+    }
 
-  private static LocalDateTime createsDateTime(String date) {
-    return LocalDateTime.parse(date, DATE_TIME_FORMATTER);
-  }
-
-  private static ProcessPart createsProcessPart(String type, String part) {
-    return new ProcessPart(type, part);
-  }
-
-  private static ProcessPart createsProcessPart(String type, String subType, String part) {
-    return new ProcessPart(type, subType, part);
-  }
-
-  private static Movement createsProcessMovement(String movementDate, String movementDescription) {
-    return new Movement(createsDate(movementDate), movementDescription);
-  }
-
-  private static DistributionDate createsDistributionDate(LocalDateTime distributionDate,
-      String distributionType) {
-    return new DistributionDate(distributionDate, distributionType);
-  }
-
-  private static LocalDate createsDate(String date) {
-    return LocalDate.parse(date, DATE_FORMATTER);
-  }
+    private static LocalDate createsDate(String date) {
+        return LocalDate.parse(date, DATE_FORMATTER);
+    }
 }
